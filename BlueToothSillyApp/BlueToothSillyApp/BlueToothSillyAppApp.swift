3a3cc4bd5d10b2cdb559ef87654b6906c1cb6e6e
Conner Maddalozzo <reach.maddaloc@gmail.com>
2022-11-29 12:30:09 -0500

    initial commit

---
+++ BlueToothSillyApp/BlueToothSillyApp/BlueToothSillyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueToothSillyAppApp.swift
+//  BlueToothSillyApp
+//
+//  Created by Conner Maddalozzo on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BlueToothSillyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
